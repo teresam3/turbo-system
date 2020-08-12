a059cf74e780d74dfaac869b904c3e2ac44df89f
Michael Shepard <michael.shepard8@gmail.com>
2020-08-11 20:47:28 -0700

    Added NewsCatcher API

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ var settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+
+// This code pulls the relevant news based on a user's provided league.
+
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://newscatcher.p.rapidapi.com/v1/search?media=True&sort_by=relevancy&lang=en&page=1&q=NFL",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "newscatcher.p.rapidapi.com",
+		"x-rapidapi-key": "441f5fff35msh66538a1f641bd16p1624c8jsnf551a30cbe66"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
